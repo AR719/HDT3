2ff027ace120d5d3519d28c5c4f9cff5bc901bd1
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2023-02-08 21:18:37 -0600

    no se que hice todo me da error ahhhhhhhhhhhhh

--- random.java
+++ random.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -55,61 +54,3 @@ public static void main(String[] args) throws IOException {
 }
 
 
-=======
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.ArrayList;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.File;
-import java.io.IOException;
-
-//https://springhow.com/java-write-csv/ copiado de
-public class random {
-
-    public static List<Integer> generateRandomArray(int n){
-        ArrayList<Integer> list = new ArrayList<Integer>(n);
-        Random random = new Random();
-        
-        for (int i = 0; i < n; i++)
-        {
-            list.add(random.nextInt(1000));
-        }
-       return list;
-    }  
-    
-
-
-public static void main(String[] args) throws IOException {
-        
- 
-    FileWriter writer = new FileWriter("C:/Users/pablo/Desktop/Estructura de datos/Git/tarea3/random/valores.csv");
-
-    List<Integer> test = generateRandomArray(3000);
-    
-    File file = new File("test.csv");
-        FileWriter fw = new FileWriter(file);
-        BufferedWriter bw = new BufferedWriter(fw);
-
-        bw.write("valores");
-        bw.newLine();
-        for(int i=0;i<test.size();i++)
-        {
-            bw.write(test.get(i+1)+","+test.get(i++));
-            bw.newLine();
-        }
-      
-        bw.close();
-        fw.close();
-
-
-}
-
-}
-
-
->>>>>>> 2dfeefdc9f7dbd436f94f9c67bbedc2efa762a01
